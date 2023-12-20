7641f732d9a1a73c39d478813e4030943b96a3c6
Anas Boustani <sino3791@gmail.com>
2023-12-20 18:58:50 +0100

    ....

---
+++ docs/search/all_1.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['binaries_20and_20output_0',['Binaries and output',['../index.html#autotoc_md5',1,'']]],
+  ['build_20from_20source_1',['Build from source',['../index.html#autotoc_md4',1,'']]],
+  ['build_20logs_2',['build logs',['../md__d_1_2home_2desktop_2github_2shgui_2_8shci_2linux_2log.html',1,'Linux build logs'],['../md__d_1_2home_2desktop_2github_2shgui_2_8shci_2windows_2log.html',1,'Windows build logs']]]
+];
